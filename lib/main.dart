18448916b7b3462125c0ceee606e12c87149676e
Augustus2001 <107476202+Augustus2001@users.noreply.github.com>
2022-07-31 15:28:29 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('calucator')),
+        appBar: AppBar(
+          title: Text('apple_calucator'),
+          backgroundColor: Color.fromARGB(255, 200, 23, 11),
+        ),
         body: Home(),
       ),
     );
@@ -24,25 +27,72 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController Quanity = TextEditingController();
-  TextEditingController price = TextEditingController();
-  TextEditingController result = TextEditingController();
+  // collect data of input
+  TextEditingController apple_numb = new TextEditingController();
+  TextEditingController price = new TextEditingController();
+  TextEditingController show = new TextEditingController();
+  //show
   @override
   void initState() {
     super.initState();
-    result.text = "ผล*ผลละ30=...บาท";
+    show.text = "quantity=x ,price per 1 =x bath, pric=x bath";
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(children: [
-      Image.asset("assets/320px-Newton's-apple.jpg", width: 200),
-      Text('calculator'),
-      TextField(
-        decoration: InputDecoration(
-            labelText: 'numb of apple', border: OutlineInputBorder()),
+    return ListView(children: [
+      Column(
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Column(children: [
+                Image.asset("assets/320px-Newton's-apple.jpg", width: 200),
+                Text('calculator'),
+                TextField(
+                  controller: apple_numb,
+                  decoration: InputDecoration(
+                    labelText: 'numb of apple',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: price,
+                  decoration: InputDecoration(
+                    labelText: 'price per 1',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {
+                      var result = double.parse(apple_numb.text) *
+                          double.parse(price.text);
+                      var price1 = double.parse(price.text);
+                      var quantity = int.parse(apple_numb.text);
+                      print("quantity=$quantity");
+                      print("Price=$result");
+                      //show show.txt
+                      setState(() {
+                        show.text =
+                            "quantity=$quantity ,price per 1 =$price1 bath, price=$result bath";
+                      });
+                    },
+                    child: Text("calculate"),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.fromLTRB(50, 20, 50, 20)),
+                      textStyle:
+                          MaterialStateProperty.all(TextStyle(fontSize: 30)),
+                    )),
+                Text(
+                  show.text,
+                  style: TextStyle(fontSize: 20),
+                )
+              ])),
+        ],
       )
-    ]));
+    ]);
   }
 }
